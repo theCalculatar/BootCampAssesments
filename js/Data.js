d49ef0cb46ee31da8f3a46b0946e5de1fe6844a8
theCalculatar <alpheusm13@gmail.com>
2024-05-05 03:07:23 +0200

    made home page use dynamic data,

---
+++ js/Data.js
@@ -0,0 +1,41 @@
+
+const name = 'Mahlane'
+const surname = 'Mabetlela'
+const bio =`Hello! I'm Alpheus Mabetlela, a computer enthusiast with a diverse set of interests and skills. My journey in the world of technology began with a BSc in Computer Science and Statistics. I thrive on exploring everything related to computers, from coding, DIY projects and chess to hitting the gym and diving into video games.
+<br><br>Proficient in Java, C++, Js, sql, and Python, I have hands-on experience working on multiple projects for myself and as freelancer with various frameworks, including Android Studio, React Native, Django, and Django Rest Framework. My passion lies in the intersection of creativity and technology, and I enjoy leveraging my programming expertise to bring innovative ideas to life and to make day to day tasks simpler for mankind
+<br><br>When not immersed in lines of code, you'll find me strategizing on the chessboard, staying active in the gym, or immersing myself in the latest video game adventures. Always up for a challenge, I embrace opportunities to learn and grow in the ever-evolving landscape of computer science.`
+
+const address = 'Mentz Mshongoville,R71 Stand no 632'
+const facebookLink = 'https://web.facebook.com/al.yas.58726823'
+const instaLink = 'https://www.instagram.com/iam.mahlane'
+const xLink = 'https://x.com'
+const image = '../assets/headshot.jpeg'
+const video = '../assets/video.mp4'
+
+const logo = 'assets/logo-light.png'
+
+const embededLink = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14590.537041707865!2d29.754185149999998!3d-23.902584549999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sza!4v1714820031775!5m2!1sen!2sza'
+
+const projects = [
+    {   image:'assets/img/medinow_screenshot',
+        title:'',
+        decription:'',
+        stacks:[]
+    },
+    {   image:'assets/img/medinow_screenshot',
+        title:'',
+        decription:'',
+        stacks:[]
+    },
+    {   image:'assets/img/medinow_screenshot',
+        title:'',
+        decription:'',
+        stacks:[]
+    },
+
+]
+
+const profile = {name, surname, image, video, bio, address, facebookLink, instaLink, xLink}
+
+export default {profile, logo,embededLink}
+
---
+++ js/index.js
@@ -0,0 +1,36 @@
+import data from "./data.js"
+
+// defining data
+const profile = data.profile
+const logo = data.logo
+const mapLink = data.embededLink
+
+// get elements from document
+const nameTag = document.getElementById('name')
+const imageTag = document.getElementById('image')
+const videoTag = document.getElementById('video')
+const bioTag = document.getElementById('bio')
+
+const addressTag = document.getElementById('address')
+const facebookTag = document.getElementById('facebook-a')
+const instaTag = document.getElementById('insta-a')
+const xTag = document.getElementById('twitter-a')
+
+const logoTag = document.getElementById('logo')
+
+const mapTag = document.getElementById('imap')
+
+//initiallizing the tags dynamically
+bioTag.innerHTML = `${profile.bio}`
+nameTag.innerHTML = `${profile.name} ${profile.surname}`
+imageTag.src = profile.image
+videoTag.src = profile.video
+
+addressTag.innerHTML = profile.address
+facebookTag.href = profile.facebookLink
+instaTag.href = profile.instaLink
+xTag.href = profile.xLink
+
+mapTag.src = mapLink
+
+logoTag.src = logo
